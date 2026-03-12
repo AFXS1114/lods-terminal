9ecb4750ebe0bdc61424b202e1311b9ec0f23d53
Sindh Karachi <karachi1409@gmail.com>
2026-03-12 23:41:54 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,21 +1,8 @@
-'use client';
-
-import { initializeApp, getApps, getApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-
-// Essential: NEXT_PUBLIC_ prefix is mandatory for client-side access in Next.js static builds
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+export const firebaseConfig = {
+  "projectId": "lods-app-845f7",
+  "appId": "1:319912452042:web:d73461122efc1b15b4f933",
+  "apiKey": "AIzaSyA9Lt714RRUT8O75x-X8pEhI_oFr1Tv3wU",
+  "authDomain": "lods-app-845f7.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "319912452042"
 };
-
-const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-
-export { app, auth, db };
